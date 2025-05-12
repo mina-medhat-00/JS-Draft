aee3953cbb0b09b8f2894dededd874fabdbbf49f
mina-medhat-00 <mina.medhat.3141@gmail.com>
2025-05-13 00:14:33 +0300

    update todo styling

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,6 +1,8 @@
 const $notes = document.querySelector("#notes-list");
 const $form = document.querySelector("#notes-form");
 const $input = document.querySelector("#notes-input");
+const $incompleteCountSpan = document.querySelector("#incomplete-count");
+const $clearCompletedBtn = document.querySelector("#clear-completed-btn");
 
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,23 +14,41 @@ $form.addEventListener("submit", (e) => {
   const deleteIcon = document.createElement("img");
   checkBtn.setAttribute("type", "checkbox");
 
-  checkBtn.addEventListener("click", () =>
-    checkBtn.checked
-      ? taskContent.classList.add("completed")
-      : taskContent.classList.remove("completed")
-  );
+  checkBtn.addEventListener("click", () => {
+    if (checkBtn.checked) {
+      li.classList.remove("incomplete");
+      li.classList.add("complete");
+      showIncomplete();
+    } else {
+      li.classList.remove("complete");
+      li.classList.add("incomplete");
+      showIncomplete();
+    }
+  });
 
   deleteBtn.addEventListener("click", () => {
     $notes.removeChild(li);
+    showIncomplete();
   });
 
   taskContent.textContent = $input.value;
-  checkIcon.src = "./images/icons-check.svg";
+  checkIcon.src = "./images/icon-check.svg";
   deleteIcon.src = "./images/icon-cross.svg";
   li.classList.add("note");
-  
+  li.classList.add("incomplete");
   deleteBtn.appendChild(deleteIcon);
   li.append(checkBtn, taskContent, deleteBtn);
   $notes.appendChild(li);
+  showIncomplete();
   $form.reset();
 });
+
+$clearCompletedBtn.addEventListener("click", () => {
+  document.querySelectorAll(".complete").forEach((item) => item.remove());
+  showIncomplete();
+});
+
+function showIncomplete() {
+  const incompleteCount = document.querySelectorAll(".incomplete");
+  $incompleteCountSpan.textContent = `${incompleteCount.length} Items left`;
+}
