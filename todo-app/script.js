e3b6fbec785af6105e988f06e709a132283682fe
mina-medhat-00 <mina.medhat.3141@gmail.com>
2025-05-14 21:35:16 +0300

    update todo functionality and folder structure

--- todo-app/script.js
+++ todo-app/script.js
@@ -3,13 +3,15 @@ const $form = document.querySelector("#notes-form");
 const $input = document.querySelector("#notes-input");
 const $incompleteCountSpan = document.querySelector("#incomplete-count");
 const $clearCompletedBtn = document.querySelector("#clear-completed-btn");
+const $allFilterBtn = document.querySelector("#all-filter-btn");
+const $activeFilterBtn = document.querySelector("#active-filter-btn");
+const $completedFilterBtn = document.querySelector("#completed-filter-btn");
 
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
   const li = document.createElement("li");
   const taskContent = document.createElement("p");
   const checkBtn = document.createElement("input");
-  const checkIcon = document.createElement("img");
   const deleteBtn = document.createElement("button");
   const deleteIcon = document.createElement("img");
   checkBtn.setAttribute("type", "checkbox");
@@ -19,10 +21,12 @@ $form.addEventListener("submit", (e) => {
       li.classList.remove("incomplete");
       li.classList.add("complete");
       showIncomplete();
+      refreshFilter();
     } else {
       li.classList.remove("complete");
       li.classList.add("incomplete");
       showIncomplete();
+      refreshFilter();
     }
   });
 
@@ -32,8 +36,7 @@ $form.addEventListener("submit", (e) => {
   });
 
   taskContent.textContent = $input.value;
-  checkIcon.src = "./images/icon-check.svg";
-  deleteIcon.src = "./images/icon-cross.svg";
+  deleteIcon.src = "./assets/icon-cross.svg";
   li.classList.add("note");
   li.classList.add("incomplete");
   deleteBtn.appendChild(deleteIcon);
@@ -48,7 +51,55 @@ $clearCompletedBtn.addEventListener("click", () => {
   showIncomplete();
 });
 
+$allFilterBtn.addEventListener("click", filterAll);
+
+$activeFilterBtn.addEventListener("click", filterActive);
+
+$completedFilterBtn.addEventListener("click", filterCompleted);
+
 function showIncomplete() {
   const incompleteCount = document.querySelectorAll(".incomplete");
   $incompleteCountSpan.textContent = `${incompleteCount.length} Items left`;
+}
+
+function filterAll() {
+  $activeFilterBtn.classList.remove("active-filter");
+  $completedFilterBtn.classList.remove("active-filter");
+  $allFilterBtn.classList.add("active-filter");
+  document.querySelectorAll(".note").forEach((note) => {
+    note.classList.remove("hidden-note");
+  });
+  console.log("filter all");
+}
+
+function filterActive() {
+  $allFilterBtn.classList.remove("active-filter");
+  $completedFilterBtn.classList.remove("active-filter");
+  $activeFilterBtn.classList.add("active-filter");
+  document.querySelectorAll(".note").forEach((note) => {
+    note.classList.remove("hidden-note");
+  });
+  document.querySelectorAll(".complete").forEach((note) => {
+    note.classList.add("hidden-note");
+  });
+  console.log("filter active");
+}
+
+function filterCompleted() {
+  $allFilterBtn.classList.remove("active-filter");
+  $activeFilterBtn.classList.remove("active-filter");
+  $completedFilterBtn.classList.add("active-filter");
+  document.querySelectorAll(".note").forEach((note) => {
+    note.classList.remove("hidden-note");
+  });
+  document.querySelectorAll(".incomplete").forEach((note) => {
+    note.classList.add("hidden-note");
+  });
+  console.log("filter completed");
+}
+
+function refreshFilter() {
+  if ($allFilterBtn.classList.contains("active-filter")) filterAll();
+  else if ($activeFilterBtn.classList.contains("active-filter")) filterActive();
+  else filterCompleted();
 }
